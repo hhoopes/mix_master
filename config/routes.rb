07947eabd94dd17d6182a0a73776af071079f8b2
hhoopes <heidih@gmail.com>
2016-02-21 21:12:59 -0700

    Add beginnings of users, Spotify authentication
    Created session controller, log in and log out, and user model, but did not
    provide tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :artists do
     resources :songs, only: [:new, :create, :index]
   end
-
   resources :songs, only: [:show]
-
   resources :playlists, only: [:index, :new, :create, :show, :edit, :update]
+  get '/auth/spotify/callback', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
